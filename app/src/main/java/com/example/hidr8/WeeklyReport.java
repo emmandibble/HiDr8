42c5ba344c929209772d73c991792abd904eeef6
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-03-16 15:46:40 -0400

    Add goBack button that returns to MainActivity

--- app/src/main/java/com/example/hidr8/WeeklyReport.java
+++ app/src/main/java/com/example/hidr8/WeeklyReport.java
@@ -2,7 +2,9 @@ package com.example.hidr8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WeeklyReport extends AppCompatActivity {
 
@@ -10,9 +12,10 @@ public class WeeklyReport extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_report);
-
-
     }
 
-
+    public void goBackOnClick(View view) {
+        Intent intent = new Intent(WeeklyReport.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
