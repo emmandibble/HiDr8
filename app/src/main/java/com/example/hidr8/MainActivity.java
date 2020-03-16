cbd4275948e1222e220fe70f5e62282dfd986006
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-03-16 13:08:40 -0400

    Add MainActivity with buttons, blank WeeklyReport
    Add contents of MainActivity from another android studio project. The
    MainActivity displays a progress bar and a clickable water bottle that
    works as a button updating the progress bar and the text above the
    progress bar. A button with the text "WEEKLY REPORT" transitions to the
    WeeklyReport activity. The activity WeeklyReport was holds a TextView
    that displays "This is the weekly report screen".
    
    The updates to MainActivity accomplishes the functionality of a
    clickable water bottle image that the user can press to update a
    progress bar that shows the user's progress towards their daily goal.
    The WeeklyReport activity will hold data about user activity and is
    one of the main screens used in the app.

--- app/src/main/java/com/example/hidr8/MainActivity.java
+++ app/src/main/java/com/example/hidr8/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.hidr8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView waterAmount;
+    ProgressBar waterBar;
+    ImageButton waterBottle;
+    Button weeklyReport;
+    double containerSize;
+    double total;
+    double incrementCount;
+    double currentAmount = 0;
+    String progressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        waterAmount = findViewById(R.id.waterAmount);
+        waterBar = findViewById(R.id.progressBar);
+        waterBottle = findViewById(R.id.WaterBottle);
+        weeklyReport = findViewById(R.id.weeklyReport);
+
+        containerSize = 8;
+        total = 80;
+        incrementCount = (containerSize/total) * 100;
+        currentAmount = 0;
+
+        progressText = (currentAmount + " fl oz / " + total + " fl oz");
+        waterAmount.setText(progressText);
+
+        weeklyReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WeeklyReport.class);
+                intent.putExtra("randomVal", 50);
+                startActivityForResult(intent, 1);
+                onActivityResult(1, 2, intent);
+            }
+        });
+
+    }
+
+    public void waterOnClick(View view) {
+        waterBar.incrementProgressBy((int)Math.round(incrementCount));
+        currentAmount += (int)Math.round(containerSize);
+        progressText = (currentAmount + " / " + total + " oz.");
+        waterAmount.setText(progressText);
     }
 }
+
