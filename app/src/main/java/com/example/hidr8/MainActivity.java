6700a87786dfeb61c74ae56e0ab00c272b204cd0
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-03-17 11:08:27 -0400

    Add hamburger button with 2 menu items

--- app/src/main/java/com/example/hidr8/MainActivity.java
+++ app/src/main/java/com/example/hidr8/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.hidr8;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -29,15 +36,64 @@ public class MainActivity extends AppCompatActivity {
     //variable used to hold the currentAmount of water that has been drank for the day
     double currentAmount = 0;
 
+    private String[] navigationDrawerItemTitles;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private CharSequence drawerTitle;
+    private CharSequence title;
+    ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //configureToolbar();
+        title = drawerTitle = getTitle();
+        navigationDrawerItemTitles = getResources().getStringArray(R.array.navigation_drawer_items_array);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerList = findViewById(R.id.left_drawer);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, navigationDrawerItemTitles);
+        drawerList.setAdapter(adapter);
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        //when first item is selected
+                        break;
+                    case 1:
+                        //when second item is selected
+                        break;
+                }
+
+
+            }
+        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.app_name, R.string.app_name);
+        //This is necessary to change the icon of the Drawer Toggle upon state change.
+        drawerToggle.syncState();
+
         waterAmountText = findViewById(R.id.waterAmount);
         waterProgressBar = findViewById(R.id.progressBar);
         waterBottle = findViewById(R.id.WaterBottle);
         weeklyReportButton = findViewById(R.id.weeklyReport);
+
+
+
 
         //defines default values
         containerSize = 8;
@@ -67,6 +123,14 @@ public class MainActivity extends AppCompatActivity {
         //increases the currentAmount based on the current containerSize
         currentAmount += (int)Math.round(containerSize);
         waterAmountText.setText(currentAmount + " fl oz / " + goal + " fl oz");
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        //actionbar.setHomeAsUpIndicator(R.drawable.ic_action_menu_white);
+        actionbar.setDisplayHomeAsUpEnabled(true);
     }
 }
 
